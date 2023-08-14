d76987378b9a7e5da71ebbfc8f7fbca9ca795cbd
Holly Stefan <hmbullis828@gmail.com>
2023-08-14 16:14:19 -0600

    product except solution O(n) time constraint

--- product_of_array_except_self.js
+++ product_of_array_except_self.js
@@ -24,7 +24,7 @@ var productExceptSelf1 = function(nums) {
 //the for-loop is only nested if the number is less than 0.
 //could still be O(n^2) if all the numbers in the array are less than 0.
 
-var productExceptSelf = function(nums) {
+var productExceptSelf2 = function(nums) {
   var total = 1;
   var result = [];
   for (var i = 0; i < nums.length; i ++) {
@@ -45,4 +45,32 @@ var productExceptSelf = function(nums) {
     }
   }
   return result;
+};
+
+
+//final attempt O(n) time complexity
+
+var productExceptSelf3 = function(nums) {
+  //create array to store the product of all numbers lower than i
+  const productBelowI = [];
+  //iterate over nums and multiply
+  for (var i = 0; i < nums.length; i++) {
+    var current = nums[i];
+    if (productBelowI.length === 0) {
+      productBelowI.push(current);
+    } else {
+      productBelowI.push(current * productBelowI[i - 1]);
+    }
+  }
+  //at the end of this for loop you'll have an array with the product of all the numbers below i
+  var current = 1;
+  var result = [];
+  //iterate backward over productBellowI
+  for (var i = nums.length-1; i > 0; i--) {
+    result.push(productBelowI[i - 1] * current);
+    //current will now store the product of all numbers greater than i;
+    current = nums[i] * current;
+  }
+  result.push(current);
+  return result.reverse();
 };
