52d523ccfa544b07f62fbe13529380bbc8684d87
Holly Stefan <hmbullis828@gmail.com>
2023-08-21 16:36:11 -0600

    three Sum initial breakdown

--- largest_sum_subarray.js
+++ largest_sum_subarray.js
@@ -8,9 +8,12 @@
 
 var maxSubArray1 = function(nums) {
   var totalSum = nums[0];
+  //add all the numbers in the array up.
   for (var i = 1; i < nums.length; i++) {
     totalSum= totalSum + nums[i];
   }
+  var maxSum = totalSum;
+  //iterate over the array from left to right
   for (var i = 0; i < nums.length; i++) {
     if (totalSum - nums[i] > maxSum) {
       maxSum = maxSum - nums[i];
@@ -19,6 +22,7 @@ var maxSubArray1 = function(nums) {
       break;
     }
   }
+  //iterate over the array from right to left
   for (var i = nums.length-1; i > -1; i--) {
     if (totalSum - nums[i] > maxSum) {
       maxSum = maxSum - nums[i];
