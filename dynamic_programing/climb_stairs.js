66780f2d8a88727e4e34f612a8314ea5ea802ba7
Holly Stefan <hmbullis828@gmail.com>
2023-08-28 14:57:19 -0600

    climbstairs1

---
+++ dynamic_programing/climb_stairs.js
@@ -0,0 +1,10 @@
+//given a number of stairs return the number of ways a person can climb the stairs.
+//you can climb stairs one or two at a time.
+
+//input: number of stairs
+//output: ways to climb the stairs
+
+//method 1 dynamically
+var climbStairs1 = function(n) {
+
+};
