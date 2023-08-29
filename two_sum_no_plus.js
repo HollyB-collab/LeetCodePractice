fafc0ce467e017cc1cc564678eb5d4a398a9f1aa
Holly Stefan <hmbullis828@gmail.com>
2023-08-29 13:02:24 -0600

    setup two sum no plus

---
+++ two_sum_no_plus.js
@@ -0,0 +1,13 @@
+//given two integers return the sum of the integers without using + or -
+
+//input: two numbers
+//output: sum of the numbers
+
+//constraints: no plus or minus
+
+//I don't think there is a math way to solve this, so there must be a program trick.
+//darn it why is it bit shifting
+
+var getSum = function(a, b) {
+
+};
