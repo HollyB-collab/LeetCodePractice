9f364e7e5652631666fb0aa271e68cf2c2f389e9
Holly Stefan <hmbullis828@gmail.com>
2023-09-05 11:34:44 -0600

    second attempt this one worked

--- pairs_chain.js
+++ pairs_chain.js
@@ -5,17 +5,15 @@
 //constraints: pairs may not be sequential
 
 var findLongestChain = function(pairs) {
-  var firstNumberArray = [];
-  for (var i = 0; i < pairs.length; i++) {
-    firstNumberArray.push(pairs[i][0]);
-  }
-  firstNumberArray = firstNumberArray.sort((a,b) => {return a - b});
-  var chainvalues = Array(firstNumberArray.length).fill(1);
-  for (var j = 1; j < firstNumberArray.length; j++) {
+  var sortedArray = pairs.sort((a, b) => {
+    return a[0] - b[0];
+  });
+  var chainvalues = Array(sortedArray.length).fill(1);
+  for (var j = 1; j < sortedArray.length; j++) {
     for (var k = j; k >= 0; k--) {
-          if (firstNumberArray[j] > firstNumberArray[k] + 1) {
-      chainvalues[j] = Math.max(chainvalues[j], chainvalues[k] +1);
-    }
+      if (sortedArray[j][0] > sortedArray[k][1]) {
+        chainvalues[j] = Math.max(chainvalues[j], chainvalues[k] +1);
+      }
     }
   }
   return Math.max(...chainvalues);
