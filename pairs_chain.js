144093353ecdaa6e62d7734bed5c9fc02ad551d6
Holly Stefan <hmbullis828@gmail.com>
2023-09-05 11:30:48 -0600

    first attempt

--- pairs_chain.js
+++ pairs_chain.js
@@ -4,3 +4,19 @@
 //output: number
 //constraints: pairs may not be sequential
 
+var findLongestChain = function(pairs) {
+  var firstNumberArray = [];
+  for (var i = 0; i < pairs.length; i++) {
+    firstNumberArray.push(pairs[i][0]);
+  }
+  firstNumberArray = firstNumberArray.sort((a,b) => {return a - b});
+  var chainvalues = Array(firstNumberArray.length).fill(1);
+  for (var j = 1; j < firstNumberArray.length; j++) {
+    for (var k = j; k >= 0; k--) {
+          if (firstNumberArray[j] > firstNumberArray[k] + 1) {
+      chainvalues[j] = Math.max(chainvalues[j], chainvalues[k] +1);
+    }
+    }
+  }
+  return Math.max(...chainvalues);
+};
