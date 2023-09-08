543eeb56e7bfcaad54a95a06d83d27baa50651e4
Holly Stefan <hmbullis828@gmail.com>
2023-09-08 09:54:13 -0600

    word_break first attempt

--- dynamic_programing/word_break.js
+++ dynamic_programing/word_break.js
@@ -4,6 +4,26 @@
 
 var wordBreak = function(s, wordDict) {
   //iterate through the string until one word is found
-  //remove first word from string
-  //iterate through string again.
+  var findWord = (string, wordDict) => {
+    for (var i = 0; i < string.length; i++) {
+      var testWord = string.slice(0, i);
+      //iterate through dictonary
+      for (var j = 0; j < wordDict.length; j++) {
+        if (testWord === wordDict[j]) {
+          string.splice(0, i);
+          //try to find another word
+          if (string.length != 0) {
+            return findWord(string, wordDict);
+          }
+          if (string.length === 0) {
+            return true;
+          }
+        }
+      }
+      if (string.length != 0 && i === string.length -1) {
+        return false;
+      }
+    }
+  }
+  findWord(s, wordDict);
 };
