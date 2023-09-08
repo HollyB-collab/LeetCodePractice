761c2e39fc5b2bcf72c7bf53699e6d0c2d03a672
Holly Stefan <hmbullis828@gmail.com>
2023-09-08 09:36:21 -0600

    setup wordbreak

---
+++ dynamic_programing/word_break.js
@@ -0,0 +1,9 @@
+//Given a string of words and a dictionary return true if the string can be split into two or more words.
+//input: string and dictionary
+//output: boolean
+
+var wordBreak = function(s, wordDict) {
+  //iterate through the string until one word is found
+  //remove first word from string
+  //iterate through string again.
+};
