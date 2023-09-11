ebc4a3b63521b6230ce050d1c3fd2177ff9cdc5b
Holly Stefan <hmbullis828@gmail.com>
2023-09-11 12:32:52 -0600

    setup palindrome problem

--- dynamic_programing/word_break.js
+++ dynamic_programing/word_break.js
@@ -2,25 +2,30 @@
 //input: string and dictionary
 //output: boolean
 
+//first attempt time complexity O(n^2)
+
 var wordBreak = function(s, wordDict) {
-  //iterate through the string until one word is found
+  //iterate through the string and pulling out letter combos
   var findWord = (string, wordDict) => {
     for (var i = 0; i < string.length; i++) {
       var testWord = string.slice(0, i);
       //iterate through dictonary
       for (var j = 0; j < wordDict.length; j++) {
+        //compare recent testword to dictonary words
+        //if testword matches pull it out of the string.
         if (testWord === wordDict[j]) {
           string.splice(0, i);
           //try to find another word
           if (string.length != 0) {
             return findWord(string, wordDict);
           }
+          //if string is empty the string was made up of words
           if (string.length === 0) {
             return true;
           }
         }
       }
-      if (string.length != 0 && i === string.length -1) {
+      if (string.length != 0 && i === string.length - 1) {
         return false;
       }
     }
---
+++ palindrome.js
@@ -0,0 +1,10 @@
+//given a number x return true if x is a palidrome.
+
+//input: number
+//output: boolean
+
+var isPalindrome = function(x) {
+  //change number to string
+  //reverse string
+  //check if original and reversed string are same
+};
