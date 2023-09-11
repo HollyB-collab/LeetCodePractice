6a0c2724e9a31b0b9951132971e0a9da3f061b9e
Holly Stefan <hmbullis828@gmail.com>
2023-09-11 12:46:47 -0600

    setup numeral obj

--- easy/roman_numerals.js
+++ easy/roman_numerals.js
@@ -4,5 +4,22 @@
 //output: number
 
 var romanToInt = function(s) {
-
+ //create object that stores the numeral values
+  var numeralValues = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+    IV: 4,
+    IX: 9,
+    XL: 40,
+    XC: 90,
+    CD: 400,
+    CM: 900,
+  };
+ //break string up into contingent values
+ //add values together
 };
