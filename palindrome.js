47642e619bbb53b67f960081169832f388d5286b
Holly Stefan <hmbullis828@gmail.com>
2023-09-11 12:37:05 -0600

    completed palindrome problem

--- palindrome.js
+++ palindrome.js
@@ -5,6 +5,13 @@
 
 var isPalindrome = function(x) {
   //change number to string
+  var originalString = x.toString();
   //reverse string
+  var reversedString = originalString.split("").reverse().join("");
   //check if original and reversed string are same
+  if (originalString === reversedString) {
+    return true;
+  } else {
+    return false;
+  }
 };
