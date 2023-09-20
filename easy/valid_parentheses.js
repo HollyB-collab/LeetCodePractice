4780ffa46e084cdacd06fdd097da9f46ec4a2cdb
Holly Stefan <hmbullis828@gmail.com>
2023-09-20 10:57:58 -0600

    problem setup

---
+++ easy/valid_parentheses.js
@@ -0,0 +1,8 @@
+//given a string containing parentheses determine if the parentheses are used in the correct order and closed.
+
+//input: string
+//output: boolean
+
+var isValid = function(s) {
+
+};
